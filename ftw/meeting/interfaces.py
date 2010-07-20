98ce6a100a01b4760760a9ee73d1fe4274f86465
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-07-20 13:16:36 +0000

    added browserlayer and remove relatedItems viewlet

--- ftw/meeting/interfaces.py
+++ ftw/meeting/interfaces.py
@@ -1,11 +1,18 @@
+from plone.theme.interfaces import IDefaultPloneLayer
 from zope.interface import Interface
+
 
 class IMeetingItem(Interface):
     """A type for meeting items."""
 
+
 class IMeeting(Interface):
     """A type for meetings."""
 
+
 class IResponsibilityInfoGetter(Interface):
-    """Inteface for ResponsibilityInfoGetter utilities
-    """
+    """Inteface for ResponsibilityInfoGetter utilities"""
+
+
+class IMeetingLayer(IDefaultPloneLayer):
+    """Themelayer for Meeting"""
