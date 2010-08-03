eb9f64c7ab403c46f184ddab5097f2fe34afc6a3
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-08-03 15:25:22 +0200

    fix problem with empty date records(https://extranet.4teamwork.ch/intranet/10-interne-projekte/4teamwork-egov/tracker-4teamwork-egov/271)

--- ftw/meeting/viewlets/generate_meeting.py
+++ ftw/meeting/viewlets/generate_meeting.py
@@ -20,19 +20,20 @@ class GenerateMeetingViewlet(ViewletBase):
         # raw result from poodletable
         # includes a new part results (counted votes per date entry)
         self.poodle_result = poodle_table.poodleResults(print_html=False)
-
-        # dict which containes
+        
+        # options for select input
         options_list = []
         # first iterate throught dates - for the right order
-        for i, date in enumerate(self.poodle_result['dates']):
-            # dates_record is the correct entry from getDates(datagridfield)
-            dates_record = self.context.getDates()[i]
-            options_list.append(
-                dict(
-                    hash=self.poodle_result['ids'][i],
-                    date=dates_record['date'],
-                    duration=dates_record['duration'],
-                    counter=self.poodle_result['result'][i]))
+        if self.poodle_result:
+            for i, date in enumerate(self.poodle_result['dates']):
+                # dates_record is the correct entry from getDates(datagridfield)
+                dates_record = self.context.getDates()[i]
+                options_list.append(
+                    dict(
+                        hash=self.poodle_result['ids'][i],
+                        date=dates_record['date'],
+                        duration=dates_record['duration'],
+                        counter=self.poodle_result['result'][i]))
 
         self.options = options_list
 
@@ -45,7 +46,12 @@ class GenerateMeetingViewlet(ViewletBase):
     def show_generate(self):
         """Decides if the viewlet will be shown or not"""
         
+        # no member
         if not self.member:
+            return False
+        
+        # no dates in poodle
+        if not self.context.getDates():
             return False
         
         return self.member.id in self.context.Creators() and not \
