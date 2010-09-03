7ba0f20f5e50d40ac81d22be342df7d4f365119c
Jonas Baumann <jone@jone.ch>
2010-09-03 10:09:56 +0200

    javascript cleanup: added some missing ";"s

--- ftw/meeting/browser/scripts/meeting_toggle_date.js
+++ ftw/meeting/browser/scripts/meeting_toggle_date.js
@@ -1,48 +1,48 @@
-   
+
 function MeetingTypeSelector(){
         var inpute_meeting_types = jq('input[name=meeting_type]');
-        
+
         var available_schematas = [];
         inpute_meeting_types.each(function(i){
             available_schematas[i] = this.value.split('_');
-            })  
-        
-        available_schematas = jq.map(available_schematas, function(n){return n})
-        
+                                  });
+
+  available_schematas = jq.map(available_schematas, function(n){return n;});
+
         // hide legends
         jq(available_schematas).each(function(i){
                 jq('#fieldsetlegend-'+this).hide();
-            })
+                                     });
 
         // hide fieldsets
         inpute_meeting_types.each(function(i){
                 jq('#fieldset-'+this).hide();
-            }) 
-            
+                                  });
+
         //show enabled schematas
         inpute_meeting_types.each(function(i){
                 if (this.checked){
-                    var schemata_to_show = this.value.split('_');   
+                    var schemata_to_show = this.value.split('_');
                         jq(schemata_to_show).each(function(i){
-                            jq('#fieldset-'+this).show()
-                        })                    
+                                                    jq('#fieldset-'+this).show();
+                                                  });
                     }
-            })
-        
+                });
+
         inpute_meeting_types.bind('click', function(e){
-            
+
                 var schemata_to_show = this.value.split('_');
-                
+
                 //hide all schematas - like a reset
                 jq(available_schematas).each(function(i){
                         jq('#fieldset-'+this).hide();
-                    })
+                });
                 //show the given schematas
                 jq(schemata_to_show).each(function(i){
                         jq('#fieldset-'+this).show();
-                    })
-                
-            })
+                });
+
+            });
     }
 
 jq(MeetingTypeSelector);
@@ -54,17 +54,17 @@ function MeetingItemToggler(){
         toggleItem.bind('click',function(e){
                 var parentItem = jq(this).closest('.MeetingItemWrapper');
                 var meetingBody = jq('#'+parentItem.attr('id') + ' .MeetingItemBody');
-                
+
                 if (meetingBody.css('display') != 'none'){
-                        jq('#'+parentItem.attr('id') + ' .MeetingItemBody').hide('blind');   
+                        jq('#'+parentItem.attr('id') + ' .MeetingItemBody').hide('blind');
                         jq('#'+parentItem.attr('id') + ' .MeetingItemHead .toggleImage').attr('src',portal_url+'/++resource++meeting-styles/arrow_right.png');
                     }
                 else {
-                        jq('#'+parentItem.attr('id') + ' .MeetingItemBody').show('slow'); 
+                        jq('#'+parentItem.attr('id') + ' .MeetingItemBody').show('slow');
                         jq('#'+parentItem.attr('id') + ' .MeetingItemHead .toggleImage').attr('src',portal_url+'/++resource++meeting-styles/arrow_down.png');
                     }
-                
-                
+
+
             });
     }
 jq(MeetingItemToggler);
