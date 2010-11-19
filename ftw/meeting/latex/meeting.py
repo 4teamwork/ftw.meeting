59a28d006aa30341534503158f714750ccca53e6
Julian Infanger <j.infanger@4teamwork.ch>
2010-11-19 08:23:55 +0100

    Issue #440 Buch PDF-Export implementieren: traktanden details

--- ftw/meeting/latex/meeting.py
+++ ftw/meeting/latex/meeting.py
@@ -3,7 +3,6 @@ from plonegov.pdflatex.browser.converter import LatexCTConverter
 
 
 class MeetingLatexConverter(LatexCTConverter):
-    
 
     def __call__(self, context, view):
         self.controller = view
@@ -16,7 +15,23 @@ class MeetingLatexConverter(LatexCTConverter):
         # define a method for easy writing
         latex = []
 
-      #  latex.append(r'' % conv(context.Title()))
+        # traktanden
+        traktanden = []
+        if len(context.getChildNodes()):
+            latex.append(r'\begin{enumerate}')
+            for brain in context.getChildNodes():
+                responsibility = [{'contact': t} for t in brain.responsibility]
+                traktanden.append(dict(
+                    title = conv(brain.Title()),
+                    Verantwortlicher = self.get_latex_responsibility(
+                        responsibility
+                        ),
+                    Text = conv(brain.text),
+                    Ergebnis = conv(brain.conclusion),
+                ))
+        latex_traktanden = ['-- %s' % t['title'] for t in traktanden]
+        latex_traktanden = '\\newline'.join(latex_traktanden)
+
         latex.append(r'\begin{tabular}{p{0.3\textwidth}p{0.7\textwidth}}')
         latex.append(r'\multicolumn{2}{l}{\textbf{%s}}\\' % conv(
             context.Title()))
@@ -37,24 +52,36 @@ class MeetingLatexConverter(LatexCTConverter):
         latex.append(self.get_row(
             'End-Zeit',
             context.toLocalizedTime(context.end(), time_only=1)))
-        latex.append(self.get_row('Verantwortliche',
-                                  self.get_latex_responsibility()))
-        latex.append(self.get_row('Beschreibung', context.Description()))
-        # traktanden
-        traktanden = []
-        for brain in context.getChildNodes():
-            traktanden.append('-- %s' % conv(brain.Title()))
         latex.append(self.get_row(
-            'Traktanden', r'\newline'.join(traktanden)))
+            'Verantwortliche',
+            self.get_latex_responsibility(self.context.getResponsibility())))
+        latex.append(self.get_row('Beschreibung', context.Description()))
+        latex.append(self.get_row('Traktanden', latex_traktanden))
         latex.append(r'\end{tabular}')
+        latex.append(r'\newline')
+        count = 0
+        for traktandum in traktanden:
+            count += 1
+            latex.append(
+                r'\begin{tabular}{p{0.3\textwidth}p{0.7\textwidth}}')
+            latex.append(r'\multicolumn{2}{l}{\ }\\')
+            latex.append(
+                r'\multicolumn{2}{l}{\textbf{%s. %s}}\\' % (
+                    count, traktandum['title']))
+            del traktandum['title']
+            for t_key in traktandum.keys():
+                latex.append(self.get_row(
+                    t_key,
+                    traktandum[t_key]))
+            latex.append(r'\end{tabular}')
         return '\n'.join(latex)
 
     def get_row(self, title, value):
         return r'\textbf{%s} & %s\\' % (title, value)
 
-    def get_latex_responsibility(self):
+    def get_latex_responsibility(self, responsibility):
         result = []
-        for term in self.context.getResponsibility():
+        for term in responsibility:
             userid = term['contact']
             mt = getToolByName(self, 'portal_membership')
             user = mt.getMemberById(userid)
