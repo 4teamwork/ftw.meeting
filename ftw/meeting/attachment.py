bb3abbf3b9570b1bd6b9f7a32831343376c04786
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-03-05 11:55:24 +0100

    added ical attachment

---
+++ ftw/meeting/attachment.py
@@ -0,0 +1,55 @@
+from Acquisition import aq_inner
+from StringIO import StringIO
+
+
+class ICSAttachmentCreator(object):
+    """An adapter to add ics-attachments.
+    Called by ftw.notificatin.email"""
+
+    def __init__(self, context):
+        self.context = aq_inner(context)
+        self.request = self.context.REQUEST
+
+    def __call__(self, object_):
+        mime_type = "text/calendar"
+        attachments = []
+        self.meeting_types = ['dates_additional',
+                              'meeting_dates_additional', ]
+
+        if object_.getMeeting_type() in self.meeting_types:
+            ical_view = getattr(object_.aq_explicit, 'ics_view', None)
+            if ical_view:
+                ical_view = ical_view(
+                    object_.REQUEST, object_.REQUEST.RESPONSE)
+                ical_view = self.set_reminder(ical_view, 30)
+                ical_file = StringIO(ical_view)
+                ical_attachment = (ical_file, 'ICal.ics', mime_type.split('/'))
+                attachments = [ical_attachment, ]
+
+        return attachments
+
+    def set_reminder(self, ical, time_in_past):
+        """set the reminder of a ical-object"""
+
+        position = ical.find("END:VEVENT")
+        if position < 0:
+            return ical
+
+        splitted = []
+        splitted.append(ical[:position])
+        splitted.append(ical[position:])
+        splitted[0] += self.get_valarm(time_in_past)
+
+        return "\r\n".join(splitted)
+
+    def get_valarm(self, time_in_past):
+        """create and return the valarm-string"""
+
+        valarm = []
+        valarm.append("BEGIN:VALARM")
+        valarm.append("TRIGGER:-PT%sM" % time_in_past)
+        valarm.append("ACTION:DISPLAY")
+        valarm.append("DESCRIPTION:Reminder")
+        valarm.append("END:VALARM")
+
+        return "\r\n".join(valarm)
