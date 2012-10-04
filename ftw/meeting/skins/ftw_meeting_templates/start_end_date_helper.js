4f1e7251d91ea58cd4201391fb6574375efc54ab
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-10-04 15:14:39 +0200

    Only auto-fill end_date if end_date is no set

--- ftw/meeting/skins/ftw_meeting_templates/start_end_date_helper.js
+++ ftw/meeting/skins/ftw_meeting_templates/start_end_date_helper.js
@@ -10,10 +10,15 @@ jq(function(){
         var month = jq('[id*=month]', $start).attr('value');
         var day = jq('[id*=day]', $start).attr('value');
         
-        // Update them on end_date field
-        jq('[id*=year]', $end).attr('value', year);
-        jq('[id*=month]', $end).attr('value', month);
-        jq('[id*=day]', $end).attr('value', day);
+        // Update them on end_date field, only if end date is empty
+        var end_year = jq('[id*=year]', $end);
+        var end_month = jq('[id*=month]', $end);
+        var end_day = jq('[id*=day]', $end);
+        if (end_year.attr('value') === '0000' && end_month.attr('value') === '00' && end_day.attr('value') === '00'){
+            jq('[id*=year]', $end).attr('value', year);
+            jq('[id*=month]', $end).attr('value', month);
+            jq('[id*=day]', $end).attr('value', day);
+        }
         
         
     });
