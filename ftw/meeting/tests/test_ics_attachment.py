a8f413155121d7ba2b4fed78880194d3b1a71696
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2019-03-12 10:49:13 +0100

    Lint test_ics_attachment.py file.

--- ftw/meeting/tests/test_ics_attachment.py
+++ ftw/meeting/tests/test_ics_attachment.py
@@ -1,15 +1,15 @@
-from ftw.meeting.testing import FTW_MEETING_INTEGRATION_TESTING
 from ftw.meeting.attachment import ICSAttachmentCreator
-import unittest2 as unittest
-from plone.app.testing import TEST_USER_ID, TEST_USER_NAME
+from ftw.meeting.testing import FTW_MEETING_INTEGRATION_TESTING
 from plone.app.testing import setRoles, login
+from plone.app.testing import TEST_USER_ID, TEST_USER_NAME
 import DateTime
+import unittest2 as unittest
 
 
 class TestIcsAttachmentView(unittest.TestCase):
-    
+
     layer = FTW_MEETING_INTEGRATION_TESTING
-    
+
     def test_ics_attachment(self):
         portal = self.layer['portal']
         setRoles(portal, TEST_USER_ID, ['Manager'])
@@ -25,5 +25,6 @@ class TestIcsAttachmentView(unittest.TestCase):
         ics = creator(meeting)
         ics_data = ics[0][0].read()
         self.assertEqual("SUMMARY:TheMeeting" in ics_data, True)
-        self.assertEqual("URL:http://nohost/plone/test/meeting" in ics_data, True)
-        self.assertEqual("BEGIN:VALARM\r\nTRIGGER:-PT30M\r\nACTION:DISPLAY\r\nDESCRIPTION:Reminder\r\nEND:VALARM" in ics_data, True)
+        self.assertIn("URL:http://nohost/plone/test/meeting", ics_data)
+        self.assertIn("BEGIN:VALARM\r\nTRIGGER:-PT30M\r\nACTION:DISPLAY\r\nDESCRIPTION:Reminder\r\nEND:VALARM",
+                      ics_data)
